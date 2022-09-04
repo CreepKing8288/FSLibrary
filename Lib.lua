f85de0b56a94638e190bba967faf25e6584a297d
CreepKing8288 <93425203+CreepKing8288@users.noreply.github.com>
2022-09-04 19:25:18 +0800

    Update Lib.lua

--- Lib.lua
+++ Lib.lua
@@ -15,6 +15,7 @@ function library:CreateWindow(text)
     Design.Name = "Design"
     Design.Parent = game.Players.LocalPlayer:WaitForChild("PlayerGui")
     Design.ZIndexBehavior = Enum.ZIndexBehavior.Sibling
+    Design.ResetOnSpawn = false
 
     WindowFrame.Name = "WindowFrame"
     WindowFrame.Parent = Design
@@ -327,11 +328,31 @@ function library:CreateWindow(text)
     end)
     end
 
+    function Window:CreateTextbox(path, text, placeholder)
+        TextBox.Name = "TextBox"
+        TextBox.Parent = HudTab[path.."hud"]
+        TextBox.BackgroundColor3 = Color3.fromRGB(77, 77, 77)
+        TextBox.Size = UDim2.new(1, 0, 0.142000005, 0)
+        TextBox.Font = Enum.Font.SourceSans
+        TextBox.TextColor3 = Color3.fromRGB(222, 222, 222)
+        TextBox.TextScaled = true
+        TextBox.TextSize = 14.000
+        TextBox.TextWrapped = true
+        TextBox.Text = text
+        TextBox.PlaceholderText = placeholder
+    end
+
     function Window:CreateNotification(title, message)
         game:GetService("StarterGui"):SetCore("SendNotification",{
             Title = title;
             Text = message;
                     })
+    end
+    function Window:GameID(placeid, callback)
+        callback = callback or function() end
+        if game.PlaceId == placeid then
+            pcall(callback)
+        end
     end
     return Window
 end
